0ac13b5206679955f0bbf934d917ba7075bb7882
chghea <chghea@amazon.co.uk>
2023-02-17 12:06:21 +0000

    Add basic documentation (comments)

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# Welcome to your CDK TypeScript project
+# CDK Windows Line-Endings Test
 
-This is a blank project for CDK development with TypeScript.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+Minimal working example to demonstate line-endings in CDK using `ec2.InitFile.fromFileInline`.
+Deploy with `cdk deploy`.
--- lib/cfn_init_windows_test-stack.ts
+++ lib/cfn_init_windows_test-stack.ts
@@ -8,20 +8,30 @@ export class CfnInitWindowsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // We do deploy in a private subnet by default
     const vpc = new ec2.Vpc(this, 'VPC', {maxAzs: 1});
 
+    // Note the use of Windows-style line endings
+    // TestFile.txt will have the same contents as if it was created natively on Windows
+    // (we create it programmatically for easy reproduction)
     const testString = "Line 1\r\nLine 2\r\nLine 3\r\n"
     const localTestFile = "TestFile.txt"
 
-    // Write the test string to a local file for testing purposes.
+    // Write the test string to a local file for testing
     fs.writeFileSync(localTestFile, testString)
 
+    // The first two will produce mangled line-endings in Windows. The last will work perfectly, since cfn-init uses
+    // python open mode "wb" for base64 encoded, instead of "w".
+    // The first not working is kind-of acceptable (could consider it an undocumented requirement that
+    // InitFile.fromString requires universal line endings), but the second is not.
     const init = ec2.CloudFormationInit.fromElements(
         ec2.InitFile.fromString("C:\\TestFileFromString.txt", testString),
         ec2.InitFile.fromFileInline("C:\\TestFileFromFileInlineUTF.txt", localTestFile),
         ec2.InitFile.fromFileInline("C:\\TestFileFromFileInlineBase64.txt", localTestFile, {base64Encoded: true}),
     )
 
+    // Create a keypair, key material will be stored in parameter store.
+    // Use this to verify the issue
     const cfnKeyPair = new ec2.CfnKeyPair(this, 'MyCfnKeyPair', {
       keyName: 'NewKeyPair',});
 
@@ -34,6 +44,7 @@ export class CfnInitWindowsTestStack extends cdk.Stack {
     }
     );
 
+    // Use with session manager or fleet manager RDP
     instance.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"))
   }
 }
